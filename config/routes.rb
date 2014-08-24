9b0181507b905c6d9e1764f3b70a24bed542b668
charanya <charanya140207@gmail.com>
2014-08-24 19:55:46 +0530

    Added controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 InviteApp::Application.routes.draw do
+  get "pages/home"
+  get "pages/invite"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

c82f190f23e4728c56ae921595e2a1a597f583f9
charanya <charanya140207@gmail.com>
2014-08-28 01:16:20 +0530

    added mailer class

---
+++ app/mailers/invitation.rb
@@ -0,0 +1,10 @@
+class Invitation < ActionMailer::Base
+  default from: "charanya140207@gmail.com"
+
+  def invitation_mail(email_id)
+  	@email_id=email_id
+  	 @url  = 'http://example.com/login'
+  	mail(:to => @email_id, :subject => "Invitation from from  wwww.somesite.com")
+  	
+  end
+end

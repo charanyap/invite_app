f1b4947dc6a449b1ea021ccbee094c924702d77e
charanya <charanya140207@gmail.com>
2014-09-10 22:58:42 +0530

    cleared error

--- config/environment.rb
+++ config/environment.rb
@@ -4,14 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 InviteApp::Application.initialize!
 
-ActionMailer::Base.smtp_settings = {
-  :user_name => 'your_sendgrid_username',
-  :password => 'your_sendgrid_password',
-  :domain => 'yourdomain.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
-
 
